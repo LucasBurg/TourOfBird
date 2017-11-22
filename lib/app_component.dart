5b5d1476ec53cfce87a0c6b3138ff28934c0ed87
Lucas Daniel Burg Mota <lucas.mota@ielusc.br>
2017-11-22 16:01:37 -0200

    Create a class to build Birds

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,19 @@
 import 'package:angular/angular.dart';
 
+class Bird {
+  final int id;
+  String name;
+
+  Bird(this.id, this.name);
+}
+
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  template: '<h1>{{title}}</h1><h2>{{bird.name}}</h2>',
 )
+
 class AppComponent {
-  var name = 'Angular';
+  final title = 'Tour Of Bird';
+  
+  Bird bird = new Bird(1, 'Canário');
 }
