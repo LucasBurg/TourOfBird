051e9e4d8d757cefa07bf2c62b98f7409815dee5
Lucas Daniel Burg Mota <lucas.mota@ielusc.br>
2017-11-23 08:28:15 -0200

    Template now is the file external

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,13 +1,6 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 
-class Bird {
-  final int id;
-  String name;
-
-  Bird(this.id, this.name);
-}
-
 @Component(
   selector: 'my-app',
 
@@ -15,20 +8,21 @@ class Bird {
   //template: '<h1>{{title}}</h1><h2>{{bird.name}}</h2>',
 
   // multi line
-  template: '''
-    <h1>{{title}}</h1>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="bird.name" placeholder="name">
-    </div>
-    <h2>Detalhes de {{bird.name}}</h2>
-    <div>
-      <label>Id: </label>{{bird.id}}
-    </div>
-    <div>
-      <label>Nome: </label>{{bird.name}}
-    </div>
-  ''',
+  //template: '''
+  //  <h1>{{title}}</h1>
+  //  <h2>Detalhes de {{bird.name}}</h2>
+  //  <div>
+  //    <label>Id: </label>{{bird.id}}
+  //  </div>
+  //  <div>
+  //    <label>name: </label>
+  //    <input [(ngModel)]="bird.name" placeholder="name">
+  //  </div>
+  //''',
+
+  //url ref
+  templateUrl: 'app_component.html',
+
   directives: const [formDirectives],
 )
 
@@ -36,4 +30,11 @@ class AppComponent {
   final title = 'Tour Of Bird';
   
   Bird bird = new Bird(1, 'Canário');
+}
+
+class Bird {
+  final int id;
+  String name;
+
+  Bird(this.id, this.name);
 }
