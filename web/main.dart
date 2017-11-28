77a8bcd969fe6531c5ffcfc358bb305ed40fde15
Lucas Daniel Burg Mota <lucas.mota@ielusc.br>
2017-11-28 14:45:58 -0200

    init the module http

--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,12 @@
 import 'package:angular/angular.dart';
 import 'package:angular_app/app_component.dart';
 import 'package:angular_router/angular_router.dart';
+import 'package:http/browser_client.dart';
 
 void main() {
   bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
     provide(LocationStrategy, useClass: HashLocationStrategy),
+    provide(BrowserClient, useFactory: () => new BrowserClient(), deps: []),
   ]);
 }
